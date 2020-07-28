afbb3f918be29370faff7577e9881c439167eded
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-07-28 21:19:49 +0600

    Add files via upload

---
+++ LAB-5.2[PHP]/Forget.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XCompany</title>
+</head>
+<body>
+   
+		<fieldset>
+			<h1>XComany</h1>
+		
+		<table>
+			<tr>
+				<td>Welcome to XCompany <a href="Home.php"> Home</a>  <a href="Login.php"> Login</a> <a href="Registration.php">Registration</a></td>
+				
+			</tr>
+            <tr>
+                <td>
+                    <fieldset>
+    <legend><b>Forget Password</b></legend>
+	<form>
+		<br/>
+		<table width="100%" cellpadding="0" cellspacing="0">
+			
+			<tr>
+				<td>Email</td>
+				<td>:</td>
+				<td>
+					<input name="email" type="text">
+					<abbr title="hint: sample@example.com"><b>i</b></abbr>
+				</td>
+				<td></td>
+			</tr>
+            
+            </table>
+		<hr/>
+		<input type="submit" value="Submit">
+		<a href="Forget.php"> Forget Password</a>
+	</form>
+</fieldset>
+                </td>
+            </tr>
+			
+			<tr>
+				<td><footer> Copyright &copy; 2017</footer>
+</td>
+				
+			</tr>
+            
