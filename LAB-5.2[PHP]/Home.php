22a43fc37267d644475097d92b662ef91cdb8bb8
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-07-28 21:20:42 +0600

    Add files via upload

---
+++ LAB-5.2[PHP]/Home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XCompany</title>
+</head>
+<body>
+    <form>
+   
+		<fieldset>
+			<h1>XComany</h1>
+		
+		<table>
+			<tr>
+				<td>Welcome to XCompany          <a href="Home.php"> Home</a>  <a href="Login.html"> Login</a> <a href="Registration.html">Registration</a></td>
+				
+			</tr>
+			
+			<tr>
+				<td><footer> Copyright &copy; 2017</footer>
+</td>
+				
+			</tr>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+		</table>
+		</fieldset>
+	</form>
+</body>
+</html>
