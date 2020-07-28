207e568c0b5d1ece328ca245eaa88811c6085009
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-07-28 21:22:42 +0600

    Add files via upload

---
+++ LAB-5.2[PHP]/Login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XCompany</title>
+</head>
+<body>
+   
+		<fieldset>
+			<h1>XComany</h1>
+		
+		<table>
+			<tr>
+				<td>Welcome to XCompany          <a href="Home.php"> Home</a>  <a href="Login.php"> Login</a> <a href="Registration.php">Registration</a></td>
+				
+			</tr>
+            <tr>
+                <td>
+                    <fieldset>
+    <legend><b>Log In</b></legend>
+	<form>
+		<br/>
+		<table width="100%" cellpadding="0" cellspacing="0">
+			<tr>
+				<td>Name</td>
+				<td>:</td>
+				<td><input name="name" type="text"></td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td>Password:</td>
+				<td>:</td>
+				<td>
+					<input name="Password" type="password">
+					
+				</td>
+				<td></td>
+			</tr>
+            <tr>
+                <td>
+                    <input type="checkbox" value="lsRememberMe" name="name"> <label for="rememberMe">Remember me</label>
+                </td>
+            </tr>
+            </table>
+		<hr/>
+		<input type="submit" value="Submit">
+		<a href="Forget.php"> Forget Password</a>
+	</form>
+</fieldset>
+                </td>
+            </tr>
+			
+			<tr>
+				<td><footer> Copyright &copy; 2017</footer>
+</td>
+				
+			</tr>
+            </table>
+    </fieldset>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+            
