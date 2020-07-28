718677a36b9d7f920e84287a03536d8043231d8c
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-07-28 21:27:13 +0600

    Add files via upload

---
+++ LAB-5.2[PHP]/Registration.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XCompany</title>
+</head>
+<body>
+    <form>
+   
+		<fieldset>
+			<h1>XComany</h1>
+		
+		<table>
+			<tr>
+				<td>Welcome to XCompany          <a href="Home.php"> Home</a>  <a href="Login.php"> Login</a> <a href="Registration.php">Registration</a></td>
+				
+			</tr>
+            <tr>
+                <td>
+                    <fieldset>
+    <legend><b>REGISTRATION</b></legend>
+	<form>
+		<br/>
+		<table width="100%" cellpadding="0" cellspacing="0">
+			<tr>
+				<td>Name</td>
+				<td>:</td>
+				<td><input name="name" type="text"></td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td>Email</td>
+				<td>:</td>
+				<td>
+					<input name="email" type="text">
+					<abbr title="hint: sample@example.com"><b>i</b></abbr>
+				</td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td>User Name</td>
+				<td>:</td>
+				<td><input name="userName" type="text"></td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input name="password" type="password"></td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td>Confirm Password</td>
+				<td>:</td>
+				<td><input name="confirmPassword" type="password"></td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td colspan="3">
+					<fieldset>
+						<legend>Gender</legend>    
+						<input name="gender" type="radio">Male
+						<input name="gender" type="radio">Female
+						<input name="gender" type="radio">Other
+					</fieldset>
+				</td>
+				<td></td>
+			</tr>		
+			<tr><td colspan="4"><hr/></td></tr>
+			<tr>
+				<td colspan="3">
+					<fieldset>
+						<legend>Date of Birth</legend>    
+						<input name= "date" type="text" size="2" />/
+						<input name ="month" type="text" size="2" />/
+						<input name ="year" type="text" size="4" />
+						<font size="2"><i>(dd/mm/yyyy)</i></font>
+					</fieldset>
+				</td>
+				<td></td>
+			</tr>
+		</table>
+		<hr/>
+		<input type="submit" value="Submit">
+		<input type="reset">
+	</form>
+</fieldset>
+                </td>
+            </tr>
+			
+			<tr>
+				<td><footer> Copyright &copy; 2017</footer>
+</td>
+				
+			</tr>      
+            
+		</table>
+		</fieldset>
+	</form>
+</body>
+</html>
+
