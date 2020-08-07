6f598345e6ce7490ddc4262acad582efde6290b1
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-08-08 01:22:50 +0600

    Add files via upload

---
+++ Mid_Project/logcheck.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_POST['submit']))
+{
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $db="intern";
+    $username = $_POST['userName'];
+    $password = $_POST['password'];
+    $conn= mysqli_connect($host,$user,$pass,$db);
+    
+    $result = mysqli_query($conn,"select * from intern where User_Name ='$username' and Password ='$password'");
+    
+    $rows = mysqli_num_rows($result);
+    echo $rows;
+    if(!empty($rows))
+    {
+        session_start();
+        $_SESSION['intern']='true';
+        echo"Correct User name and password";
+        header("refresh:1; url=home.html");
+        
+    }
+    else{
+        echo"User Name or Password is Wrong ";
+        header("refresh:2; url=log.html");
+
+    }
+    
+}
+?>
