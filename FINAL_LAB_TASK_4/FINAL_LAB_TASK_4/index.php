c17455d9424eeea4463ae939b9f493ff370dc27b
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-09-01 09:19:15 +0600

    Add files via upload

---
+++ FINAL_LAB_TASK_4/FINAL_LAB_TASK_4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index  Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<h1 id='head2'>This is our first page</h1>
+	<a href="views/login.php">login</a> 
+	<a href="views/register.php">SignUp</a>
+
+	
+
+</body>
+</html>
